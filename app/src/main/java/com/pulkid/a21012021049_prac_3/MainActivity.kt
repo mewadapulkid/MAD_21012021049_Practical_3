970d6fd305bf47523adc64d4d1f977abf9ced534
Pulkid <pulkidmewada21@gnu.ac.in>
2023-09-27 21:45:16 +0530

    Initial commit

---
+++ app/src/main/java/com/pulkid/a21012021049_prac_3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.pulkid.a21012021049_prac_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
